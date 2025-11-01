62b300a60436cf66a99ab171417d8c729a58da24
Manav Parekh <manav13p@gmail.com>
2025-11-01 21:12:21 +0530

    fix: Set correct Vite base path for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // --- ADD THIS LINE ---
+  base: '/', 
+  // --------------------
+
   plugins: [react()],
   server: {
     proxy: {
-      // This proxies any request starting with /api
-      // to your backend server at http://localhost:5000
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
